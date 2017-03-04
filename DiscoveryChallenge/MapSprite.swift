8d9304781608c13e09fb321c6d6804797fda2634
Alberto Volpe <volpea.93@gmail.com>
2017-03-04 19:04:56 +0100

    map sprite

---
+++ DiscoveryChallenge/MapSprite.swift
@@ -0,0 +1,55 @@
+//
+//  MapSprite.swift
+//  DiscoveryChallenge
+//
+//  Created by Alberto Volpe on 04/03/2017.
+//  Copyright © 2017 Alberto Volpe. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+import CoreMotion
+
+class MapSprite: Sprite {
+    
+    static let downScaleAndStop = SKAction.init(named: "DownScaleAndStop")!
+    static let upScaleAndStop = SKAction.init(named: "UpScaleAndStop")!
+    //static let interactionSound = SKAction.playSoundFileNamed("ding.wav", waitForCompletion: true);
+    private let TAG = "MapSprite"
+    
+    override init(usingImage assetName: String, referredWithName name: String, withAlpha alpha: CGFloat, onLayer z: CGFloat) {
+        
+        super.init(usingImage: assetName, referredWithName: name, withAlpha: alpha, onLayer: z)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(TAG, "touchesBegan")
+        //run(interactionSound)
+        run(MapSprite.upScaleAndStop, withKey: "fadeInOut")
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print(TAG, "touchesEnded")
+        //run(interactionSound)
+        run(MapSprite.downScaleAndStop, withKey: "fadeInOut")
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        print(TAG, "touchesMoved")
+        for t in touches {
+            let positionInScene = t.location(in: self)
+            let previousPosition = t.previousLocation(in: self)
+            let translation = CGPoint(x: positionInScene.x - previousPosition.x, y: positionInScene.y - previousPosition.y)
+            
+            position = CGPoint(x: position.x + translation.x, y: position.y + translation.y)
+        }
+    }}
+
